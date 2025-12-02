2152e34593554a20bbca1f84be522e13f743ec9a
sofusl90 <sofus@laubel.net>
2025-12-02 16:05:24 +0100

    added cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Debug, Clone, ValueEnum)]
+enum OutputMode {
+    Dzn,
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    #[arg(long, value_enum)]
+    output_mode: Option<OutputMode>,
+
+    #[arg(long)]
+    output_objective: bool,
+
+    #[arg(short = 'f')]
+    ignore_search: bool,
+
+    #[arg(short = 'p')]
+    threads: Option<usize>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.output_mode {
+        Some(OutputMode::Dzn) => println!("Mode set to DZN"),
+        None => println!("No output mode specified"),
+    }
+
+    if args.output_objective {
+        println!("Outputting objective value...");
+    }
+
+    if args.ignore_search {
+        println!("-f flag received. Search strategy ignored (or acknowledged).");
+    }
+
+    if let Some(n) = args.threads {
+        println!("Running with {} threads", n);
+    } else {
+        println!("Running with default threads");
+    }
 }
