998ed4085ee8a3cf2f2e23485beb4572552ab08f
Jonas Bork <jonas10bork@gmail.com>
2025-12-04 11:10:50 +0100

    Implement solver_output from json

--- src/solver_output.rs
+++ src/solver_output.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-use serde_json::Value;
+use serde_json::{Map, Value};
 
 #[derive(Debug)]
 pub enum Output {
@@ -10,7 +10,7 @@ pub enum Output {
 
 #[derive(Debug, PartialEq)]
 pub enum Status {
-    Done,
+    OptimalSolution,
     Unsatisfiable,
     Unbounded,
     Unknown,
@@ -19,20 +19,21 @@ pub enum Status {
 #[derive(Debug)]
 pub struct Solution {
     pub solution: String,
-    pub objective: Option<i32>,
+    pub objective: i64,
 }
 
 impl Status {
     const UNSATISFIABLE_STR: &str = "UNSATISFIABLE";
     const UNBOUNDED_STR: &str = "UNBOUNDED";
     const UNKNOWN_STR: &str = "UNKNOWN";
+    const OPTIMAL_SOLUTION_STR: &str = "OPTIMAL_SOLUTION";
 }
 
-#[derive(Debug)] // TODO: maybe remove debug?
+#[derive(Debug)]
 pub enum ParseError {
     JsonParsing(serde_json::Error),
     MissingObjective,
-    JsonMalformed(String),
+    Field(String),
 }
 
 impl From<serde_json::Error> for ParseError {
@@ -56,18 +57,89 @@ impl Output {
 
     pub fn parse(output: &str) -> Result<Self, ParseError> {
         let Value::Object(json) = serde_json::from_str(output)? else {
-            return Err(ParseError::JsonMalformed(
-                "Outermost JSON is not an object".to_owned(),
-            ));
+            return Err(ParseError::Field("Output is not a JSON object".to_owned()));
         };
 
-        let Some(Value::String(kind)) = json.get("type") else {
-            return Err(ParseError::JsonMalformed(
-                "type field is missing from json or it is not a string".to_owned(),
-            ));
-        };
+        let kind = parse_string_field(&json, "type")?;
 
-        todo!()
+        match kind.as_str() {
+            "solution" => Ok(Self::Solution(parse_solution(&json)?)),
+            "status" => Ok(Self::Status(parse_status(&json)?)),
+            _ => Err(ParseError::Field(format!(
+                "'type' = '{kind}' is not supported"
+            ))),
+        }
+    }
+}
+
+fn parse_solution(json: &Map<String, Value>) -> Result<Solution, ParseError> {
+    let output = parse_object_field(&json, "output")?;
+    let solution = parse_string_field(&json, "default")?;
+    let objective = parse_i64_field(&json, "_objective")?;
+
+    Ok(Solution {
+        solution: solution.clone(),
+        objective,
+    })
+}
+
+fn parse_status(json: &Map<String, Value>) -> Result<Status, ParseError> {
+    let status = parse_string_field(json, "status")?;
+    match status.as_str() {
+        Status::OPTIMAL_SOLUTION_STR => Ok(Status::OptimalSolution),
+        Status::UNSATISFIABLE_STR => Ok(Status::Unsatisfiable),
+        Status::UNBOUNDED_STR => Ok(Status::Unbounded),
+        Status::UNKNOWN_STR => Ok(Status::Unknown),
+        _ => Err(ParseError::Field(format!(
+            "'status' = '{status}' is an unknown status"
+        ))),
+    }
+}
+
+fn parse_field<'a>(json: &'a Map<String, Value>, field: &str) -> Result<&'a Value, ParseError> {
+    match json.get(field) {
+        Some(value) => Ok(value),
+        None => Err(ParseError::Field(format!(
+            "field '{field}' is missing from json"
+        ))),
+    }
+}
+
+fn parse_string_field<'a>(
+    json: &'a Map<String, Value>,
+    field: &str,
+) -> Result<&'a String, ParseError> {
+    match parse_field(json, field)? {
+        Value::String(value) => Ok(value),
+        _ => Err(ParseError::Field(format!(
+            "field '{field}' is not a string"
+        ))),
+    }
+}
+
+fn parse_i64_field(json: &Map<String, Value>, field: &str) -> Result<i64, ParseError> {
+    match parse_field(json, field)? {
+        Value::Number(value) => match value.as_i64() {
+            Some(num) => Ok(num),
+            None => Err(ParseError::Field(format!(
+                "field '{field}' is a number but not an i64"
+            ))),
+        },
+        _ => Err(ParseError::Field(format!(
+            "field '{field}' is not a number"
+        ))),
+    }
+}
+
+fn parse_object_field<'a>(
+    json: &'a Map<String, Value>,
+    field: &str,
+) -> Result<&'a Map<String, Value>, ParseError> {
+    match parse_field(json, field)? {
+        Value::Object(value) => Ok(value),
+        _ => Err(ParseError::Field(format!(
+            "field '{field}' is not an object"
+        ))),
     }
 }
 
@@ -75,16 +147,12 @@ impl Output {
 mod tests {
     use super::*;
 
-    const ARITHEMETIC_TARGET_OUTPUT: &str = r#"{"type": "solution", "output": {"default": "yCoor = [29, 1, 8, 6, 31, 15, 11, 6, 6, 1, 42, 11, 40, 26, 37, 16, 16, 43, 21, 33];\nS = [22, 41, 29];\nD = 45;\nobjective = 137;\n", "raw": "yCoor = [29, 1, 8, 6, 31, 15, 11, 6, 6, 1, 42, 11, 40, 26, 37, 16, 16, 43, 21, 33];\nS = [22, 41, 29];\nD = 45;\nobjective = 137;\n", "json": {  "yCoor" : [29, 1, 8, 6, 31, 15, 11, 6, 6, 1, 42, 11, 40, 26, 37, 16, 16, 43, 21, 33],  "objective" : 137,  "S" : [22, 41, 29],  "D" : 45,  "_objective" : 137}}, "sections": ["default", "raw", "json"]}
-{"type": "solution", "output": {"default": "yCoor = [29, 1, 8, 6, 31, 15, 11, 6, 6, 1, 42, 11, 40, 26, 37, 16, 43, 16, 21, 33];\nS = [22, 44, 14];\nD = 45;\nobjective = 125;\n", "raw": "yCoor = [29, 1, 8, 6, 31, 15, 11, 6, 6, 1, 42, 11, 40, 26, 37, 16, 43, 16, 21, 33];\nS = [22, 44, 14];\nD = 45;\nobjective = 125;\n", "json": {  "yCoor" : [29, 1, 8, 6, 31, 15, 11, 6, 6, 1, 42, 11, 40, 26, 37, 16, 43, 16, 21, 33],  "objective" : 125,  "S" : [22, 44, 14],  "D" : 45,  "_objective" : 125}}, "sections": ["default", "raw", "json"]}
-{"type": "solution", "output": {"default": "yCoor = [29, 1, 9, 7, 31, 15, 11, 6, 7, 1, 42, 11, 40, 26, 37, 16, 43, 16, 21, 33];\nS = [21, 44, 14];\nD = 45;\nobjective = 124;\n", "raw": "yCoor = [29, 1, 9, 7, 31, 15, 11, 6, 7, 1, 42, 11, 40, 26, 37, 16, 43, 16, 21, 33];\nS = [21, 44, 14];\nD = 45;\nobjective = 124;\n", "json": {  "yCoor" : [29, 1, 9, 7, 31, 15, 11, 6, 7, 1, 42, 11, 40, 26, 37, 16, 43, 16, 21, 33],  "objective" : 124,  "S" : [21, 44, 14],  "D" : 45,  "_objective" : 124}}, "sections": ["default", "raw", "json"]}
-{"type": "solution", "output": {"default": "yCoor = [29, 1, 8, 6, 31, 11, 33, 6, 6, 1, 43, 11, 41, 26, 12, 16, 18, 15, 21, 37];\nS = [22, 42, 15];\nD = 43;\nobjective = 122;\n", "raw": "yCoor = [29, 1, 8, 6, 31, 11, 33, 6, 6, 1, 43, 11, 41, 26, 12, 16, 18, 15, 21, 37];\nS = [22, 42, 15];\nD = 43;\nobjective = 122;\n", "json": {  "yCoor" : [29, 1, 8, 6, 31, 11, 33, 6, 6, 1, 43, 11, 41, 26, 12, 16, 18, 15, 21, 37],  "objective" : 122,  "S" : [22, 42, 15],  "D" : 43,  "_objective" : 122}}, "sections": ["default", "raw", "json"]}
-{"type": "solution", "output": {"default": "yCoor = [29, 1, 8, 6, 31, 11, 33, 6, 6, 1, 43, 11, 41, 26, 12, 16, 15, 18, 21, 37];\nS = [22, 42, 14];\nD = 43;\nobjective = 121;\n", "raw": "yCoor = [29, 1, 8, 6, 31, 11, 33, 6, 6, 1, 43, 11, 41, 26, 12, 16, 15, 18, 21, 37];\nS = [22, 42, 14];\nD = 43;\nobjective = 121;\n", "json": {  "yCoor" : [29, 1, 8, 6, 31, 11, 33, 6, 6, 1, 43, 11, 41, 26, 12, 16, 15, 18, 21, 37],  "objective" : 121,  "S" : [22, 42, 14],  "D" : 43,  "_objective" : 121}}, "sections": ["default", "raw", "json"]}
-{"type": "solution", "output": {"default": "yCoor = [29, 1, 9, 7, 31, 11, 33, 6, 7, 1, 43, 11, 41, 26, 12, 16, 15, 18, 21, 37];\nS = [21, 42, 14];\nD = 43;\nobjective = 120;\n", "raw": "yCoor = [29, 1, 9, 7, 31, 11, 33, 6, 7, 1, 43, 11, 41, 26, 12, 16, 15, 18, 21, 37];\nS = [21, 42, 14];\nD = 43;\nobjective = 120;\n", "json": {  "yCoor" : [29, 1, 9, 7, 31, 11, 33, 6, 7, 1, 43, 11, 41, 26, 12, 16, 15, 18, 21, 37],  "objective" : 120,  "S" : [21, 42, 14],  "D" : 43,  "_objective" : 120}}, "sections": ["default", "raw", "json"]}
-{"type": "status", "status": "UNKNOWN"}"#;
+    const ARITHEMETIC_TARGET_SOLUTION: &str = r#"{"type": "solution", "output": {"default": "yCoor = [29, 1, 8, 6, 31, 15, 11, 6, 6, 1, 42, 11, 40, 26, 37, 16, 16, 43, 21, 33];\nS = [22, 41, 29];\nD = 45;\nobjective = 137;\n", "raw": "yCoor = [29, 1, 8, 6, 31, 15, 11, 6, 6, 1, 42, 11, 40, 26, 37, 16, 16, 43, 21, 33];\nS = [22, 41, 29];\nD = 45;\nobjective = 137;\n", "json": {  "yCoor" : [29, 1, 8, 6, 31, 15, 11, 6, 6, 1, 42, 11, 40, 26, 37, 16, 16, 43, 21, 33],  "objective" : 137,  "S" : [22, 41, 29],  "D" : 45,  "_objective" : 137}}, "sections": ["default", "raw", "json"]}"#;
+    const ARITHMETIC_TARGET_STATUS: &str = r#"{"type": "status", "status": "UNKNOWN"}"#
 
     // #[test]
-    // fn test_get_first_number() {
+    // fn test_parse_solution() {
+    //     let input = ARITHEMETIC_TARGET_SOLUTION;
     //     let num = get_first_number("hel 654; uhte\nueou");
     //     assert_eq!(num, Some("654".to_owned()))
     // }
