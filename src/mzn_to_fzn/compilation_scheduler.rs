930443392bfa448178d5a766b3b61f4dadefb0e0
Jonas Bork <59069012+jonas-bork@users.noreply.github.com>
2026-02-27 13:57:04 +0100

    Fix compilation scheduler over core budget error (#83)

--- src/mzn_to_fzn/compilation_scheduler.rs
+++ src/mzn_to_fzn/compilation_scheduler.rs
@@ -232,22 +232,26 @@ impl State {
     /// Precondition: The solver should be started.
     pub fn register_main_compilation(&mut self, solver: SolverId, cores: Cores) {
         logging::info!("registering main compilation for solver '{solver}' with cores '{cores}'");
-        self.used_cores += 1;
         self.available_cores += cores;
 
         let running_compilation = self.running_compilations.remove(&solver);
-        let priority = match running_compilation {
-            None => self.extra_compilations_queue.remove_by_solver_id(&solver),
-            Some(RunningCompilation::Extra(priority)) => Some(priority),
+        let (new_cores, priority) = match running_compilation {
+            None => {
+                self.used_cores += 1;
+                (
+                    cores,
+                    self.extra_compilations_queue.remove_by_solver_id(&solver),
+                )
+            }
+            Some(RunningCompilation::Extra(priority)) => (cores, Some(priority)),
             Some(RunningCompilation::Main(old_compilation)) => {
-                self.available_cores = self.available_cores.saturating_sub(old_compilation.cores);
-                old_compilation.priority
+                (old_compilation.cores + cores, old_compilation.priority)
             }
         };
 
         self.running_compilations.insert(
             solver,
-            RunningCompilation::Main(MainCompilation::new(cores, priority)),
+            RunningCompilation::Main(MainCompilation::new(new_cores, priority)),
         );
     }
 
